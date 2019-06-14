6c63ec5bbc25086cc122e60ef22304de49506e29
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-06-14 12:06:22 +0100

    Return multiple values

--- MultipleStuff/Program.cs
+++ MultipleStuff/Program.cs
@@ -6,7 +6,35 @@ namespace MultipleStuff
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declare variables
+            double min, max;
+
+            SuperList.MinMax valores;
+
+            Tuple<double, double> t;
+
+            // Instanciar Superlist
+            SuperList list = new SuperList() {-24, 3, 4.5, -2.3, -100.3, 23};
+
+            // Get min and max
+            list.GetMinMax(out min, out max);
+
+            valores = list.GetMinMax();
+
+            t = list.GetMinMax2();
+
+            // Print out first result with first method overload
+            Console.WriteLine($"{min} and {max}");
+
+            Console.WriteLine();
+
+            // Print out second result with second method overload
+            Console.WriteLine($"{valores.Min} and {valores.Max}");
+
+            Console.WriteLine();
+
+            // Print out third result with third method overload with tuples
+            Console.WriteLine($"{t.Item1} and {t.Item2}");
         }
     }
 }
---
+++ MultipleStuff/SuperList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleStuff
+{
+    internal class SuperList : List<double>
+    {
+
+        public struct MinMax
+        {
+            // --------------Properties
+
+            // Get min value
+            public double Min { get; }
+
+            // Get max value
+            public double Max { get; }
+
+            // Constructors
+            public MinMax(double min, double max)
+            {
+                Min = min;
+                Max = max;
+            }
+        }
+
+        /// <summary>
+        /// Get in and max value of list
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        public void GetMinMax(out double min, out double max)
+        {
+            // Sort list
+            Sort();
+
+            // Get first element of list
+            min = this[0];
+
+            // Get last  element of list
+            max = this[Count -1];
+        }
+
+        /// <summary>
+        /// Get Multiple values with struct
+        /// </summary>
+        /// <param name="s"></param>
+        public MinMax GetMinMax()
+        {
+            // Declare variable
+            double min, max;
+
+            // Get minmax that return min and max value
+            GetMinMax(out min, out max);
+
+            // New instance of struct
+            return new MinMax(min, max);
+        }
+
+        /// <summary>
+        /// Save multiples values and print them out
+        /// </summary>
+        /// <returns> Min and Max value</returns>
+        public Tuple<double, double> GetMinMax2()
+        {
+            double min, max;
+            GetMinMax(out min, out max);
+
+            return new Tuple<double, double>(min, max);
+        }
+
+       
+    }
+}
