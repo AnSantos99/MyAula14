f19fc337a1daedd18105098fced8f6a079f0704b
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-06-14 12:39:50 +0100

    Stack<string> Push and Pop and create file(Ana.txt)

---
+++ WriteStuff/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WriteStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string>text = new Stack<string>();
+            string input;
+
+            do
+            {
+                input = Console.ReadLine();
+                text.Push(input);
+                
+
+            }
+            while (input != "");
+            text.Pop();
+            File.WriteAllLines("Ana.txt", text);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
